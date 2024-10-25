c44204b09cb0e578462b07c89a8011c12f0c68c5
Tao Lundolos <taow80@users.noreply.github.com>
2024-10-25 14:40:13 -0500

    Update getCommits.js

--- .github/release-notes/getCommits.js
+++ .github/release-notes/getCommits.js
@@ -2,26 +2,27 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 
 const getCommits = async () => {
-    core.info(process.argv[2]);
     const releaseVersion = process.argv[2];
 
     let myOutput = '';
     let myError = '';
 
-    const options = {};
-    options.listeners = {
+    const options = {
+        silent: true,
+        listeners: {
     stdout: (data) => {
         myOutput += data.toString();
     },
     stderr: (data) => {
         myError += data.toString();
     }
-    };
+    }
+};
 
     await exec.exec('git', ['log', `--pretty=format:%s`, `HEAD...${releaseVersion}`], options);
     if(myError){
         core.error(myError)
     } 
-    return JSON.stringify(myOutput);
+    core.exportVariable('COMMITS', JSON.stringify(myOutput));
 };
 getCommits();
