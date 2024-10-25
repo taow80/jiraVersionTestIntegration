05462412d433ac58197b6cf748ce6157b8a8b6af
Tao Lundolos <taow80@users.noreply.github.com>
2024-10-25 15:11:18 -0500

    Update getCommits.js

--- .github/release-notes/getCommits.js
+++ .github/release-notes/getCommits.js
@@ -1,6 +1,38 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
+
+const filterCommits = async (application, commits) => {
+    const commitArray = commits.split(/\r?\n/);
+        let releaseProjects = [
+            'core',
+            'domain',
+            'domain-components',
+            'fdl',
+            'omega',
+            'policy',
+            'presentation'
+        ];
+
+        let filteredCommits = [];
+        const projectRegex = /\((.+)\):/;
+        
+        if (application === 'channel') {
+          //  core.info(`ℹ️ Filtering commits for Channel and PWA`);
+            releaseProjects.push('channel', 'pwa', 'branding');
+            //const appRegex = new RegExp(`\\((\\bchannel|\\bpwa)\\):`, 'mi');
+        } else if(application === 'backoffice') {
+          // core.info(`ℹ️ Filtering commits for Backoffice`);
+            releaseProjects.push('backoffice', 'backoffice-v2');
+        }
+        filteredCommits = commitArray.filter(commit => {
+            project = commit.match(projectRegex);
+            return(project && releaseProjects.includes(project[1].toLowerCase()))
+        });
+        return filteredCommits.join("\r\n");
+
+}
+
 const getCommits = async () => {
     const releaseVersion = process.argv[2];
 
@@ -8,10 +40,9 @@ const getCommits = async () => {
     let myError = '';
 
     const options = {
-        silent: true,
         listeners: {
     stdout: (data) => {
-        myOutput += data.toString();
+        myOutput += filterCommits(application, data.toString());
     },
     stderr: (data) => {
         myError += data.toString();
@@ -23,6 +54,5 @@ const getCommits = async () => {
     if(myError){
         core.error(myError)
     } 
-    core.exportVariable('COMMITS', JSON.stringify(myOutput));
 };
 getCommits();
