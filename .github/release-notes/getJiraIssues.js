c8420156358008c48293d5536fffb3bc6d6fe70f
Tao Lundolos <taow80@users.noreply.github.com>
2024-10-28 11:08:34 -0500

    Update getJiraIssues.js

--- .github/release-notes/getJiraIssues.js
+++ .github/release-notes/getJiraIssues.js
@@ -4,16 +4,27 @@ const exec = require('@actions/exec');
 
 const getPrNumbers = async () => {
     const commits = process.argv[2];
-    console.log(`Submit Commits: ${commits}`);
+
     const prNumberRegex = /\(#(\d+)\)/g;
     const prNumberMatches = [...commits.matchAll(prNumberRegex)];
-    console.log(`PR Number Match Array:`);
-    console.log(prNumberMatches);
+    
+    let prDetailErrors = '';
     prNumberMatches.forEach(async (match) => {
-        console.log(`Loop match: ${match}`);
-      const prNumber = match[1];
-      const prDetails = await exec.exec(`gh pr view ${prNumber}`);
-      console.log(`PR Details: ${prDetails}`);
+        const prNumber = match[1];
+        let prDetails = '';
+        
+        const options = {
+            listeners: {
+                stdout: (data) => {
+                    prDetails += data.toString();
+                },
+                stderr: (data) => {
+                    prDetailErrors += data.toString();
+                }
+            }
+        };
+          await exec.exec(`gh pr view ${prNumber}`, [] options);
+          console.log(`PR Details: ${prDetails}`);
     });
 
 // core.setOutput("APP_SPECIFIC_JIRA_ISSUES", appFilteredCommits);
