c9f89adbb8286dd8dbe1d83b669829af48e22c91
Tao Lundolos <taow80@users.noreply.github.com>
2024-10-28 10:00:58 -0500

    Create getJiraIssues.js

---
+++ .github/release-notes/getJiraIssues.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+
+const getPrNumbers = async () => {
+    const commits = process.argv[2]
+
+    const prNumberRegex = /\(#(\d+)\)\n/g
+    const prNumberMatches = [...commits.matchAll(prNumberRegex)];
+
+    prNumberMatches.foreach(async (match) => {
+      const prNumber = match[1];
+      const prDetails = await exec.exec(`gh pr view ${prNumber}`);
+      console.log(prDetails);
+    });
+
+core.setOutput("APP_SPECIFIC_JIRA_ISSUES", appFilteredCommits);
+    console.log(`App Filtered Commits: ${appFilteredCommits}`);
+};
+
+  
+getPrNumbers();
