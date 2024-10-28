816f6f8bd0ad6638ef304c41061f5dbc81835f17
Tao Lundolos <taow80@users.noreply.github.com>
2024-10-28 14:06:02 -0500

    Update getJiraIssues.js

--- .github/release-notes/getJiraIssues.js
+++ .github/release-notes/getJiraIssues.js
@@ -30,7 +30,7 @@ const getPrNumbers = async () => {
     
         console.log(`PR Details:`);
         console.log(prDetails);
-        const jiraIssueRegex = new RegExp(`^\-\-.*\\[(${ jiraProjectKey }\-\\d+)\\]\\(.*^## What It Does`, "g");
+        const jiraIssueRegex = new RegExp(`^\-\-.*\\[(${ jiraProjectKey }\-\\d+)\\]\\(`, "gm");
         while ((jiraIssueMatches = jiraIssueRegex.exec(prDetails)) !== null) {
             console.log('MATCHED', jiraIssueMatches);
             jiraIssueIds.push(jiraIssueMatches[1]);
